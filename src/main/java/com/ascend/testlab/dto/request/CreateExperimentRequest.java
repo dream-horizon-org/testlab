8cff6b89534a512f52c2d60efb84306a9c02e687
ravi-dreampay <ravi.pandey@dream11.com>
2025-10-10 17:38:57 +0530

    Added request and response

--- src/main/java/com/ascend/testlab/dto/request/CreateExperimentRequest.java
+++ src/main/java/com/ascend/testlab/dto/request/CreateExperimentRequest.java
@@ -4,6 +4,7 @@ import com.ascend.testlab.constants.ExperimentStatus;
 import com.ascend.testlab.constants.ExperimentType;
 import com.ascend.testlab.constants.HealthStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import java.sql.Timestamp;
 import java.util.Map;
@@ -13,49 +14,72 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateExperimentRequest {
 
+  @JsonProperty("tenant_id")
   private byte[] tenantId;
 
+  @JsonProperty("experiment_id")
   private byte[] experimentId;
 
-  @NotBlank private String name;
+  @JsonProperty("name") @NotBlank
+  private String name;
 
-  @NotBlank private String description;
+  @JsonProperty("description") @NotBlank
+  private String description;
 
+  @JsonProperty("hypothesis")
   private String hypothesis;
 
+  @JsonProperty("rules_json")
   private Map<String, Object> rulesJson;
 
+  @JsonProperty("status")
   private ExperimentStatus status;
 
+  @JsonProperty("actuals")
   private String actuals;
 
+  @JsonProperty("start_time")
   private long startTime;
 
+  @JsonProperty("end_time")
   private long endTime;
 
+  @JsonProperty("cohort_id")
   private String cohortId;
 
+  @JsonProperty("distribution_strategy")
   private String distributionStrategy;
 
+  @JsonProperty("assignment_domain")
   private String assignmentDomain;
 
+  @JsonProperty("percentage_distribution")
   private String percentageDistribution;
 
+  @JsonProperty("exposure")
   private int exposure;
 
+  @JsonProperty("created_by")
   private long createdBy;
 
+  @JsonProperty("threshold")
   private long threshold;
 
+  @JsonProperty("is_exclusive")
   private Boolean isExclusive;
 
+  @JsonProperty("health")
   private HealthStatus health;
 
+  @JsonProperty("type")
   private ExperimentType type;
 
+  @JsonProperty("created_at")
   private Timestamp createdAt;
 
+  @JsonProperty("updated_at")
   private Timestamp updatedAt;
 
+  @JsonProperty("name_tokens")
   private String nameTokens;
 }
