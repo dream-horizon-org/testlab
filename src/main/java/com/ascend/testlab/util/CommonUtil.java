33e82ec29b81e2ab8abcd356624f8d8594c1ea65
ravi-dreampay <ravi.pandey@dream11.com>
2025-11-06 15:34:21 +0530

    removed file

--- src/main/java/com/ascend/testlab/util/CommonUtil.java
+++ src/main/java/com/ascend/testlab/util/CommonUtil.java
@@ -1,22 +1,24 @@
 package com.ascend.testlab.util;
 
-import com.ascend.testlab.constants.Constants;
-import com.ascend.testlab.constants.datadog.DDConstants;
 import io.vertx.core.impl.cpu.CpuCoreSensor;
 import lombok.experimental.UtilityClass;
 
+/**
+ * Utility class for common utilities.
+ *
+ * @author Nikhil Tummidi
+ * @version 1.0
+ * @since 1.0
+ */
 @UtilityClass
 public final class CommonUtil {
 
+  /**
+   * Get the number of cores available.
+   *
+   * @return the number of cores available
+   */
   public static int getNumberOfCores() {
     return CpuCoreSensor.availableProcessors();
-  }
-
-  public static String getCircuitBreakerAspect(String aspect) {
-    return DDConstants.CB_METRIC + Constants.SPACE + aspect;
-  }
-
-  public static String getCircuitBreakerTag(String circuitBreakerName) {
-    return DDConstants.CB_NAME + Constants.COLON + circuitBreakerName;
   }
 }
