98388300aca895035f22174ce34552f7bca0730c
anudeepreddy20 <anudeep.reddy@dream11.com>
2025-11-25 15:40:07 +0530

    [updated]row mapper for experiment

--- src/main/java/com/ascend/testlab/service/impl/AllocationServiceImpl.java
+++ src/main/java/com/ascend/testlab/service/impl/AllocationServiceImpl.java
@@ -374,8 +374,8 @@ public class AllocationServiceImpl implements AllocationService {
                           String key =
                               allocation.getExperimentId().toString()
                                   + Constants.COLON
-                                  + allocation.getVariant().getDisplayName();
-                          variantCountMap.put(key, allocation.getVariant().getDisplayName());
+                                  + allocation.getVariantName();
+                          variantCountMap.put(key, allocation.getVariantName());
                         }
 
                         return allocationDAO
@@ -514,10 +514,8 @@ public class AllocationServiceImpl implements AllocationService {
     Map<String, String> variantCountMap = new HashMap<>();
     for (UserExperimentMap allocation : carryoverAssignments) {
       String key =
-          allocation.getExperimentId().toString()
-              + Constants.COLON
-              + allocation.getVariant().getDisplayName();
-      variantCountMap.put(key, allocation.getVariant().getDisplayName());
+          allocation.getExperimentId().toString() + Constants.COLON + allocation.getVariantName();
+      variantCountMap.put(key, allocation.getVariantName());
     }
 
     return allocationDAO
