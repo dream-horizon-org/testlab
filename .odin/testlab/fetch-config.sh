76f15105d8a775705102e2acd42d9224ed69f799
anudeepreddy20 <anudeep.reddy@dream11.com>
2025-12-02 21:53:42 +0530

    chore:refactor

--- .odin/testlab/fetch-config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id ${SERVICE_NAME} --output json | jq -c '.SecretString | fromjson')
-SECRETS=$(echo $SECRET_JSON | jq -r "to_entries | map(\"export \(.key)=\(.value | tostring)\") | .[]")
-
-echo $SECRETS > .config
